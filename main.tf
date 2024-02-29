7239e52b8315dcfb1bf31630bcaf97f781988770
Prakash Kumar <prakash.thelight@gmail.com>
2024-02-28 21:14:08 -0800

    Add VPC, Subnet, Internet Gateway, Route Table

---
+++ main.tf
@@ -0,0 +1,105 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIA3FLD5GCN5HQLCT5V"
+  secret_key = "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"
+}
+
+# 1. Create a VPC
+resource "aws_vpc" "prod_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "prod-vpc"
+  }
+}
+
+# 2. Create an internet gateway and attach it to the VPC
+resource "aws_internet_gateway" "prod_igw" {
+  vpc_id = aws_vpc.prod_vpc.id
+  tags = {
+    Name = "prod-igw"
+  }
+}
+
+# 3. Create a route table
+resource "aws_route_table" "prod_rt" {
+  vpc_id = aws_vpc.prod_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.prod_igw.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.prod_igw.id
+  }
+
+  tags = {
+    Name = "prod-rt"
+  }
+}
+
+# 4. Create a Subnet
+resource "aws_subnet" "subnet_1" {
+  vpc_id            = aws_vpc.prod_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "prod-subnet"
+  }
+}
+
+# 5. Associate the route table with the subnet
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet_1.id
+  route_table_id = aws_route_table.prod_rt.id
+}
+
+# 6. Create Security Group to allow port 22,80,443
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow Web inbound traffic"
+  vpc_id      = aws_vpc.prod_vpc.id
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_web"
+  }
+}
+
+
+# resource "aws_instance" "my-first-ec2-server" {
+#   ami           = "ami-008fe2fc65df48dac"
+#   instance_type = "t2.micro"
+# }
